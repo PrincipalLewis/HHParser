8c3c2f01bdd16ce4fb22bb0ced6d2f1d33d6fcf8
Vados <dflz666@gmail.com>
2015-07-17 19:47:43 +0300

    auth

---
+++ js/client_auth.js
@@ -0,0 +1,22 @@
+var search = window.location.search;
+var code = search.split('=');
+
+if (code[0] === '?code') {
+  $.ajax({
+    type: 'POST',
+    url: 'http://localhost:1337/code',
+    data: code[1],
+    success: function(msg) {
+      console.log(msg);
+      var obj = JSON.parse(msg);
+      document.getElementById('1').innerHTML = '';
+      document.getElementById('1').innerHTML = msg;
+    },
+    error: function(msg) {
+      document.getElementById('body').innerHTML =
+          '' + msg.status + ' ' + msg.statusText + '<br>' +
+          msg.responseText;
+    }
+  });
+}
+console.log(code[1]);
